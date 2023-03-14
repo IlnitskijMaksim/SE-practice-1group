2dd90bf2a1fc74f4ef7493c77edaf596c3d82969
Katerina Anurieva <kanureva.mail@gmail.com>
2023-03-14 20:26:31 +0200

    some functional

--- src/script.js
+++ src/script.js
@@ -1,6 +1,54 @@
-// $(".regular").slick({
-//     dots: true,
-//     infinite: true,
-//     slidesToShow: 3,
-//     slidesToScroll: 3
-//   });
+$(".single-item").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+
+  const slider = document.querySelector('.slider');
+  const selectedImage = document.querySelector('.selected-image');
+
+  slider.addEventListener('click', (event) => {
+      if (event.target.tagName === 'IMG') {
+          const src = event.target.dataset.src;
+          selectedImage.innerHTML = `<img src="${src}" />`;
+      }
+  });
+
+
+  function validateForm() {
+    const title = document.getElementById("title").value.trim();
+    const duration = document.getElementById("duration").value.trim();
+    const surnameTeacher = document.getElementById("surname-teacher").value.trim();
+    const surnameStudent = document.getElementById("surname-student").value.trim();
+
+    if (title === "" || duration === "" || surnameTeacher === "" || surnameStudent === "") {
+      alert("Будь ласка, заповніть всі поля");
+      return false;
+    }
+
+    if (isNaN(duration)) {
+      alert("Тривалість має бути числом.");
+      return false;
+    } else if (duration <= 0) {
+      alert("Тривалість має бути додатним числом");
+      return false;
+    }
+
+    const regex = /^[a-zA-Zа-яА-ЯІіЇїЄєҐґ'.\s,;-]+$/;
+    if (!regex.test(surnameTeacher) || !regex.test(surnameStudent)) {
+      alert("Поля вводу прізвища та ініціалів можуть містити лише літери, пробіли, апостроф і деякі розділові знаки.");
+      return false;
+    }
+
+    return true;
+  }
+
+  const createButton = document.getElementById("create");
+  createButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (validateForm()) {
+      document.getElementById("form").submit();
+    }
+  });
