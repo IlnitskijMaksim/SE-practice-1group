7cea107d53d551af712693d85b9fa589f6f8ff1c
Katerina Anurieva <113555695+katushhiaa@users.noreply.github.com>
2023-03-17 19:15:05 +0200

    Add files via upload

--- docs/script.js
+++ docs/script.js
@@ -4,72 +4,105 @@ $(".single-item").slick({
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-  });
+});
 
-  const slider = document.querySelector('.slider');
-  const selectedImage = document.querySelector('.selected-image');
+class Course {
+    constructor() {
+        this.title = document.getElementById('title').value.trim();
+        this.duration = document.getElementById('duration').value.trim();
+    }
+}
 
-  slider.addEventListener('click', (event) => {
-      if (event.target.tagName === 'IMG') {
-          const src = event.target.dataset.src;
-          selectedImage.innerHTML = `<img src="${src}" />`;
-          document.getElementById("template").className=event.target.dataset.class;
-      }
-  });
+class Teacher {
+    constructor() {
+        this.name = document.getElementById('surname-teacher').value.trim();
+    }
+}
 
+class Participant {
+    constructor() {
+        this.name = document.getElementById('surname-student').value.trim();
+    }
+}
 
-  function validateForm() {
-    const date = new Date().toLocaleDateString('en-GB');
-    const title = document.getElementById("title").value.trim();
-    const duration = document.getElementById("duration").value.trim();
-    const surnameTeacher = document.getElementById("surname-teacher").value.trim();
-    const surnameStudent = document.getElementById("surname-student").value.trim();
+class Certificate {
+    constructor() {
+        this.dateOfIssue = new Date().toLocaleDateString('en-GB');
+    }
+}
 
-    if (title === "" || duration === "" || surnameTeacher === "" || surnameStudent === "") {
-      alert("Будь ласка, заповніть всі поля");
-      return false;
+function chooseTemplate(event) {
+    const selectedImage = document.querySelector('.selected-image');
+    const containerPreview = document.getElementById("container-preview");
+
+    if (event.target.tagName === 'IMG') {
+        const src = event.target.dataset.src;
+        selectedImage.innerHTML = `<img src="${src}" />`;
+        document.getElementById("template").className = event.target.dataset.class;
+        containerPreview.classList.add("visible");
+    }
+}
+
+function generateCertificate() {
+    const course = new Course();
+    const teacher = new Teacher();
+    const participant = new Participant();
+    const certificate = new Certificate();
+
+    if (course.title === "" || course.duration === "" || teacher.name === "" || participant.name === "") {
+        alert("Будь ласка, заповніть всі поля");
+        return;
     }
 
-    const regex = /^[a-zA-Zа-яА-ЯІіЇїЄєҐґ'.\s,;-]+$/;
-    if (!regex.test(surnameTeacher) || !regex.test(surnameStudent)) {
-      alert("Поля вводу прізвища та ініціалів можуть містити лише літери, пробіли, апостроф і деякі розділові знаки.");
-      return false;
+    const regex = /^[a-zA-Zа-яА-ЯІіЇїЄєҐґ'.\s,-]+$/;
+    if (!regex.test(teacher.name) || !regex.test(participant.name)) {
+        alert("Поля вводу прізвища та ініціалів можуть містити лише літери, пробіли, апостроф і деякі розділові знаки");
+        return;
     }
 
-    return {surnameStudent, title, surnameTeacher, date, duration};
-  }
-
-  const createButton = document.getElementById("create");
-  createButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    const data = validateForm();
-    console.log(data)
-    if (data) {
-      document.getElementsByClassName("student-temp")[0].innerText=data.surnameStudent;
-      document.getElementsByClassName("title-temp")[0].innerText=data.title;
-      document.getElementsByClassName("teacher-temp")[0].innerText=data.surnameTeacher;
-      document.getElementsByClassName("date-temp")[0].innerText = data.date;
-      document.getElementsByClassName("duration-temp")[0].innerText=data.duration;
-      
-      //document.getElementById("form").submit();
+    if (course.duration <= 0) {
+        alert("Тривалість курсу має бути додатнім числом");
+        return;
     }
-  });
 
- /* Print
-  $(document).ready(function () {
-    // rest of the code
-  
-    $("#print").click(function () {
-      // modify this line to only print the selected-image div
-      var printContents = $(".selected-image").html(); 
-  
-      var originalContents = document.body.innerHTML;
-  
-      document.body.innerHTML = printContents;
-  
-      window.print();
-  
-      document.body.innerHTML = originalContents;
+    document.getElementsByClassName("student-temp")[0].innerText = participant.name;
+    document.getElementsByClassName("title-temp")[0].innerText = course.title;
+    document.getElementsByClassName("teacher-temp")[0].innerText = teacher.name;
+    document.getElementsByClassName("date-temp")[0].innerText = certificate.dateOfIssue;
+    document.getElementsByClassName("duration-temp")[0].innerText = course.duration;
+}
+
+function printCertificate() {
+    const course = new Course();
+    const teacher = new Teacher();
+    const participant = new Participant();
+
+    if (course.title === "" || course.duration === "" || teacher.name === "" || participant.name === "") {
+        alert("Будь ласка, спочатку заповніть всі поля форми");
+        return;
+    }
+
+    document.getElementById("container-preview").classList.add("print-content");
+    window.print();
+    document.getElementById("container-preview").classList.remove("print-content");
+}
+
+function downloadCertificate() {
+    window.jsPDF = window.jspdf.jsPDF;
+    const participant = new Participant();
+
+    let docPDF = new jsPDF({
+        orientation: 'landscape',
     });
-  });*/
-  
+
+    let elementHTML = document.querySelector("#template");
+    docPDF.html(elementHTML, {
+        callback: function(docPDF) {
+            docPDF.save(participant.name);
+        },
+        x: 0,
+        y: 0,
+        width: 170,
+        windowWidth: 650
+    });
+}
