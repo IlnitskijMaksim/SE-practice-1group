e58ceed60a52ade12d3bd1ac3407a2d07b065c6d
vkrychevska <valeriya.kawai65@gmail.com>
2023-03-17 20:51:50 +0200

    small changes

--- src/script.js
+++ src/script.js
@@ -102,7 +102,7 @@ function downloadCertificate() {
         },
         x: 0,
         y: 0,
-        width: 170,
-        windowWidth: 650
+        width: 300,
+        windowWidth: 700
     });
 }
